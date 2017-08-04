2111349ea15b2f44bddfd918590315f2a1ff864d
mosborne@davies.com.au <markdosborne@gmail.com>
2017-08-05 06:22:34 +1000

    Update doco in js files

--- simon/js/index.js
+++ simon/js/index.js
@@ -1,35 +1,36 @@
-/* If it floats, it's a Twitch
+/* Simon
 
 TODO:
 
-DONE:
-
-----------
-Objective: Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/Myvqmo/.
+Objective: Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/obYBjE.
 
 Fulfill the below user stories. Use whichever libraries or APIs you need. Give it your own personal style.
 
-User Story: I can see whether Free Code Camp is currently streaming on Twitch.tv.cd ..
+User Story: I am presented with a random series of button presses.
 
-User Story: I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
+User Story: Each time I input a series of button presses correctly, I see the same series of button presses but with an additional step.
 
-User Story: if a Twitch user is currently streaming, I can see additional details about what they are streaming.
+User Story: I hear a sound that corresponds to each button both when the series of button presses plays, and when I personally press a button.
 
-User Story: I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.
+User Story: If I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
 
-Hint: See an example call to Twitch.tv's JSONP API at http://forum.freecodecamp.org/t/use-the-twitchtv-json-api/19541.
+User Story: I can see how many steps are in the current series of button presses.
 
-Hint: The relevant documentation about this API call is here: https://dev.twitch.tv/docs/v5/reference/streams/#get-stream-by-user.
+User Story: If I want to restart, I can hit a button to do so, and the game will return to a single step.
 
-Hint: Here's an array of the Twitch.tv usernames of people who regularly stream: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+User Story: I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
 
-UPDATE: Due to a change in conditions on API usage explained here Twitch.tv now requires an API key, but we've built a workaround. Use https://wind-bow.gomix.me/twitch-api instead of twitch's API base URL (i.e. https://api.twitch.tv/kraken ) and you'll still be able to get account information, without needing to sign up for an API key.
+User Story: I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
+
+Hint: Here are mp3s you can use for each button: https://s3.amazonaws.com/freecodecamp/simonSound1.mp3, https://s3.amazonaws.com/freecodecamp/simonSound2.mp3, https://s3.amazonaws.com/freecodecamp/simonSound3.mp3, https://s3.amazonaws.com/freecodecamp/simonSound4.mp3.
 
 Remember to use Read-Search-Ask if you get stuck.
 
 When you are finished, click the "I've completed this challenge" button and include a link to your CodePen.
 
 You can get feedback on your project by sharing it with your friends on Facebook.
+
+DONE:
 
 */  
   var listStreams = function() {
