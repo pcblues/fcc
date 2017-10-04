94eff58bff52a9d26c03e93cfa871ddf05d9831e
mosborne@davies.com.au <markdosborne@gmail.com>
2017-10-04 16:54:43 +1100

    Working on tomato

--- tomato/js/index.js
+++ tomato/js/index.js
@@ -30,92 +30,67 @@ When you are finished, click the "I've completed this challenge" button and incl
 You can get feedback on your project by sharing it with your friends on Facebook.
 
 DONE:
-
+Display activity length
+Set activity length
+Display break length
+Set break length
+Display time
+Count down time
+Reset clock
+Start/Pause clock
 */  
-  var listStreams = function() {
-    addLog("Listing streams")
-   
-  addLog("Running getJSON...");
-  var CORS = "https://cors-anywhere.herokuapp.com/"
-  var twitchBaseURL = "https://wind-bow.gomix.me/twitch-api/"
-  var twitchURL = CORS+twitchBaseURL
-  var twitchTV = "https://www.twitch.tv/"
-  var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin432","comster404324"]
+var activityLength = 25
+var breakLength = 5
+var timeLeft = 0
 
-  $(".mo-results").html("")
-  $.each(streamers,function(x,streamer) { 
-    
-    var thisTwitchChannel = twitchURL+"channels/"+streamer+"/"
-    addLog(thisTwitchChannel);
-  
-  $.getJSON( thisTwitchChannel,{},
-  function(result) 
-     {
-      addLog("Done 1...")
-      addLog(result)  
-      var channel = result
-      var channelName = ""
-      var channelGame = ""
-      var channelStatus = ""
-      var channelLogoURL = ""
-      var channelFollowers = ""
-      var channelViewers = ""
-      if (channel) {
-        channelName = channel.display_name
-        channelGame = channel.game
-        channelStatus = channel.status
-        channelViews = channel.views
-        channelFollowers = channel.followers
-        channelLogoURL = channel.logo
-      }
-      // links.channel and links.self need left/twitchURLing.
-      //var resultBlock = "<a href=\""+links+"\" target=\"_blank\">"
-      var resultBlock = "<div class=\"mo-box\">"
-        resultBlock += "<b>"+streamer+"</b>"
-      if (channelGame) {
-        resultBlock += " - " + channelGame
-      }
-        resultBlock +="<br>"
-      if (channelLogoURL) {
-              resultBlock += "<img class=\"mo-logo\"src=\""+channelLogoURL+"\"></img><br>  "
-      }
-      if (channelStatus=="404") {
-        resultBlock += "User does not exist<br>"
-      }
-      else if (channelStatus) {        
-        resultBlock += "Views: "+channelViews+ " Followers: "+channelFollowers
-        resultBlock += "<a href=\""+twitchTV+streamer+"\" target=\"_blank\"><p>"+channelStatus+"</p></a>"
-      } else {
-        resultBlock+="Offline<BR>"
-      }
-      resultBlock+="</div>"
-      $(".mo-results").append(resultBlock) 
-       
-     })
-   .done(
-     //function() {addLog("Done 2...");}
-   )
-  .fail(
-         function( jqXHR, textStatus, errorThrown )
-         {
-          var errText = "Fail...<BR>"+textStatus+"<BR>"+errorThrown+"<BR>"
-           addLog(errText)
-        } 
-  )
-  .always(     
-    //function() {addLog("Always...");}
-  )
-  })
-
-  }
 
 $(document).ready(function() {
+
+  updateScreen()
+  document.getElementById("mo-breakUp").addEventListener("click",breakUp);// addeventlistener
+  document.getElementById("mo-breakDown").addEventListener("click",breakDown  );// addeventlistener  
+  document.getElementById("mo-reset").addEventListener("click",reset);// addeventlistener
+  document.getElementById("mo-remaining").addEventListener("click",remaining  );// addeventlistener  
+  document.getElementById("mo-timeUp").addEventListener("click",timeUp);// addeventlistener
+  document.getElementById("mo-timeDown").addEventListener("click",timeDown  );// addeventlistener  
   addLog("Document Ready")
-  listStreams()
+  
+  
 })
 
+var breakUp=function() {
+
+}
+
+var breakDown=function() {
+  
+}
+
+var timeUp=function() {
+  
+}
+
+var timeDown=function() {
+  
+}
+
+var remaining=function() {
+  
+}
+
+var reset=function() {
+  
+}
+
+
+var updateScreen = function() {
+  $(".mo-breakTime").html(breakLength)
+  $(".mo-timeStart").html(activityLength)
+  $(".mo-remaining").html(timeLeft)
+}
+
 var addLog = function(msg) {
-  //$(".mo-log").append(msg+"<BR>")
+  $(".mo-log").append(msg+"<BR>")
   console.log(msg)
 }
 
