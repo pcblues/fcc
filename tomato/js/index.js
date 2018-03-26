17192bfb2837ce8799d0650b417dfcf0c27c8b3f
Mark D Osborne <markosborne@pcblues.com>
2018-03-26 17:57:45 +1100

    Start in Windows

--- tomato/js/index.js
+++ tomato/js/index.js
@@ -1,6 +1,8 @@
 /* Tomato
 
 TODO:
+Develop in Windows
+
 Display activity length
 Set activity length
 Display break length
@@ -14,19 +16,12 @@ Start/Pause clock
 DONE:
 
 OBJECTIVE: Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/aNyxXR/.
-
 Fulfill the below user stories. Use whichever libraries or APIs you need. Give it your own personal style.
-
 User Story: I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
-
 User Story: I can reset the clock for my next pomodoro.
-
 User Story: I can customize the length of each pomodoro.
-
 Remember to use Read-Search-Ask if you get stuck.
-
 When you are finished, click the "I've completed this challenge" button and include a link to your CodePen.
-
 You can get feedback on your project by sharing it with your friends on Facebook.
 
 DONE:
@@ -40,6 +35,9 @@ Reset clock
 Start/Pause clock
 
 TO RUN:
+WINDOWS
+
+MACOS
 Open terminal
 loadnvm
 http-server in tomato directory
