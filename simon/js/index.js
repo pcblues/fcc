82da99efb4e6e1bd29c3fa1a46499953fe87d572
Mark D Osborne <markosborne@pcblues.com>
2018-04-30 23:18:31 +1000

    Assets completed

--- simon/js/index.js
+++ simon/js/index.js
@@ -14,6 +14,7 @@ Design sounds
 - lose
 - start
 - victory
+
 Show four colours
 Start a chain of button presses
 Sound for each colour
@@ -58,86 +59,9 @@ You can get feedback on your project by sharing it with your friends on Facebook
 DONE:
 
 */  
-  var listStreams = function() {
-    addLog("Listing streams")
-   
-  addLog("Running getJSON...");
-  var CORS = "https://cors-anywhere.herokuapp.com/"
-  var twitchBaseURL = "https://wind-bow.gomix.me/twitch-api/"
-  var twitchURL = CORS+twitchBaseURL
-  var twitchTV = "https://www.twitch.tv/"
-  var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin432","comster404324"]
-
-  $(".mo-results").html("")
-  $.each(streamers,function(x,streamer) { 
-    
-    var thisTwitchChannel = twitchURL+"channels/"+streamer+"/"
-    addLog(thisTwitchChannel);
-  
-  $.getJSON( thisTwitchChannel,{},
-  function(result) 
-     {
-      addLog("Done 1...")
-      addLog(result)  
-      var channel = result
-      var channelName = ""
-      var channelGame = ""
-      var channelStatus = ""
-      var channelLogoURL = ""
-      var channelFollowers = ""
-      var channelViewers = ""
-      if (channel) {
-        channelName = channel.display_name
-        channelGame = channel.game
-        channelStatus = channel.status
-        channelViews = channel.views
-        channelFollowers = channel.followers
-        channelLogoURL = channel.logo
-      }
-      // links.channel and links.self need left/twitchURLing.
-      //var resultBlock = "<a href=\""+links+"\" target=\"_blank\">"
-      var resultBlock = "<div class=\"mo-box\">"
-        resultBlock += "<b>"+streamer+"</b>"
-      if (channelGame) {
-        resultBlock += " - " + channelGame
-      }
-        resultBlock +="<br>"
-      if (channelLogoURL) {
-              resultBlock += "<img class=\"mo-logo\"src=\""+channelLogoURL+"\"></img><br>  "
-      }
-      if (channelStatus=="404") {
-        resultBlock += "User does not exist<br>"
-      }
-      else if (channelStatus) {        
-        resultBlock += "Views: "+channelViews+ " Followers: "+channelFollowers
-        resultBlock += "<a href=\""+twitchTV+streamer+"\" target=\"_blank\"><p>"+channelStatus+"</p></a>"
-      } else {
-        resultBlock+="Offline<BR>"
-      }
-      resultBlock+="</div>"
-      $(".mo-results").append(resultBlock) 
-       
-     })
-   .done(
-     //function() {addLog("Done 2...");}
-   )
-  .fail(
-         function( jqXHR, textStatus, errorThrown )
-         {
-          var errText = "Fail...<BR>"+textStatus+"<BR>"+errorThrown+"<BR>"
-           addLog(errText)
-        } 
-  )
-  .always(     
-    //function() {addLog("Always...");}
-  )
-  })
-
-  }
 
 $(document).ready(function() {
   addLog("Document Ready")
-  listStreams()
 })
 
 var addLog = function(msg) {
