26de252c216890fd5379043ba9274fd5f9c50a5f
Mark D Osborne <markosborne@pcblues.com>
2018-09-03 23:27:23 +1000

    Where we are at

--- d3 heatmap/js/index.js
+++ d3 heatmap/js/index.js
@@ -14,7 +14,7 @@ var dataset = []
 svgWidth = 700
 svgHeight= 500
 
-const margin={top:80,right:40,bottom:70,left:60}
+const margin={top:50,right:40,bottom:70,left:100}
 const gWidth = svgWidth-margin.left-margin.right;
 const gHeight = svgHeight-margin.top-margin.bottom;
 
@@ -53,36 +53,34 @@ d3.json(url, function(err, data) {
     // x axis
     var xValue= function(d) {return d.Year}
     
-    var xScale = d3.scaleOrdinal()
-    .range([0,gWidth],0,0)
+    var xScale = d3.scaleBand()
+    .rangeRound([0,gWidth])
     .domain(ndataset.map(function(val){return val.year}))
 
-    
     var xAxis = d3.axisBottom()
     .scale(xScale)
     .tickValues(xScale.domain().filter(function(year){
-      return year%10===0
+      return year%25===0
     }))
-    .tickFormat(function(d){return d}) //todo
+    .tickFormat(function(d){return d}) 
     .tickSize(10,1)
 
     var xMap = function(d){return xScale(xValue(d))}
 
-
     // y axis
-    var yValue = function(d) {
-      return new Date(d.Seconds*1000)
-    }
-
-    var yScale = d3.scaleOrdinal()
-    //.range([gHeight+margin.bottom,margin.top])
-    .range([0,gHeight])
-    .domain([1,2,3,4,5,6,7,8,9,10,11,12])
-
+    var yScale = d3.scaleBand()
+    .rangeRound([0,gHeight],0,0)
+    .domain([0,1,2,3,4,5,6,7,8,9,10,11])
+    
     var yAxis = d3.axisLeft()
     .scale(yScale)
     .tickValues(yScale.domain())
-    .tickFormat(d3.timeFormat("%B")) // todo
+    .tickFormat(function(month) {
+      
+        var mNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+        return mNames[month];
+        
+    })
     .tickSize(10,1)
    
     var yMap = function(d) { return yScale(yValue(d))}
@@ -108,19 +106,20 @@ d3.json(url, function(err, data) {
       .attr('id','description')
       .style("border", "1px solid black");
 
-    var xAxisTrans = gHeight+margin.bottom
+    var xAxisTrans = gHeight+margin.top
+
     svg.append("g")
       .attr("id", "x-axis")
-      .attr("transform", "translate(0,"+xAxisTrans+")")
+      .attr("transform", "translate("+margin.left+","+xAxisTrans+")")
       .call(xAxis)
       .append('text')
-      .attr('transform','translate(100,100)')
+      .attr('transform','translate(0,0)')
       .style('text-anchor','middle')
       .text('Years')
   
     svg.append("g")
       .attr("id", "y-axis")
-      .attr("transform", "translate("+margin.left+",0)")
+      .attr("transform", "translate("+margin.left+","+margin.top+")")
       .call(yAxis)
       .append('text')
       .attr('transform','rotate(-90)')
@@ -134,7 +133,91 @@ d3.json(url, function(err, data) {
     .append("div")
     .attr("class", "toolTip")
     .attr("id","tooltip")
+    
+    // colors
+    // ['#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026']
+    legendColors= ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"]
+
+
+  // draw legend
+  legendColors= ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"]
+  legendWidth = 400
+  legendHeight = 300/legendColors.length
+
+  var legendX = d3.scaleLinear()
+  .domain([minTemp,maxTemp])
+  .range([0, legendWidth])
+
+  var legendThreshold = d3.scaleThreshold()
+  .domain((function(min,max,count){
+    array = []
+    step = (max-min)-count;
+    var base = min
+    for (i=1;i<count;i++) {
+      array.push(base+i*step)
+    }
+    return array
+  })(minTemp,maxTemp,legendColors.length))
+  .range(legendColors)
+
+  var legendXAxis = d3.axisBottom()
+  .scale(legendX)
+  .tickSize(10,0)
+  .tickValues(legendThreshold.domain())
+  .tickFormat(d3.format(".1f"))
+
+
+    var legend = svg.append('g')
+    .classed('legend',true)
+    .attr('id','legend')
+    .attr('transform','translate(10,10)')
+
+
+  legend.append('g')
+      .selectAll("rect")
+      .data(legendThreshold.range().map(function(color){
+        var d = legendThreshold.invertExtent(color);
+        if(d[0] == null) d[0] = legendX.domain()[0];
+        if(d[1] == null) d[1] = legendX.domain()[1];
+        return d;
+      }))
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr('id','legend')
+      .attr("transform", function(d, i) { 
+        return "translate(20," + (i+1) * 20 + ")"; })
       
+
+  // draw legend colored rectangles
+  legend.append("rect")
+      .attr("x", gWidth - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", gWidth - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { 
+      return d})
+  
+      legend.append("g")
+      .attr("transform", "translate(" + 0 + "," + legendHeight + ")")
+      .call(legendXAxis);
+  
+      //heading
+      var heading = svg.append("heading");
+      heading.append("h1")
+        .attr('id', 'title')
+        .text("Monthly Global Land-Surface Temperature");
+      heading.append("h3")
+        .attr('id', 'description')
+        .html(ndataset[0].year + " - " + ndataset[ndataset.length-1].year + ": base temperature " + nbaseline + "&#8451;");
+      
+// map
     // data
     svg.append('g')
     .classed('map',true)
@@ -154,13 +237,15 @@ d3.json(url, function(err, data) {
       return xScale(d.year);
     })
     .attr('y', function(d,i){
-      return yScale(d.month);
+      return yScale(d.month)
     })
-    .attr('width', function(d,i){
-      return xScale.range(d.year);
+    .attr('width', function(d){
+      5
+      //return xScale.range(d.year)
     })
-    .attr('height', function(d,i){
-      return yScale.range(d.month);
+    .attr('height', function(d){
+      10
+      //return yScale.range(d.month)
     })
     .on("mousemove", function(d){
           tooltip
@@ -174,35 +259,7 @@ d3.json(url, function(err, data) {
       })
     .on("mouseout", function(d){ tooltip.style("display", "none");});
 
-  // draw legend
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr('id','legend')
-      .attr("transform", function(d, i) { return "translate(20," + (i+1) * 20 + ")"; });
 
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", gWidth - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  // draw legend text
-  legend.append("text")
-      .attr("x", gWidth - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { 
-         if (d===2) {
-        result= "Suspected of Doping"
-      } else {
-        result = "Not Suspected of Doping"
-      }
-      return result})
-})
+    })
 
    
-  
